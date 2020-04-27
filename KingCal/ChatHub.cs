b5dfcd4691df55dfc201a0afd7ef1b758bbd9053
Richard K Heaton <richard.k.heaton@gmail.com>
2020-04-27 18:57:04 -0400

    Initial commit of code base.

---
+++ KingCal/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("Send", user, message);
+        }
+    }
+}
